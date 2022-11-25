fde1839ebc1be137c5d91936603730f94bd1cb7f
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-25 06:55:24 -0600

    Adding hello_world.py file

---
+++ Part-01-Basics/Chapter-02-Variables-and-Simple-Data-Types/hello_world.py
@@ -0,0 +1,10 @@
+# hello_world.py, Chapter 02, Python Crash Course
+# program prints "Hello, Python World!"
+#
+# compile and execute program with the following
+# command-line scripts:
+# $ python3 hello_world.py
+
+print("Hello, Python World!")
+
+
